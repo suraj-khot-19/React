3259f7fcea870be485eb7f7525484b74fb60d1fd
suraj khot <surajkhot19082001@gmail.com>
2024-08-03 15:53:35 +0530

    basic understanding of App.js file

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+let name = "Suraj";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>trying to print name variable <b>name</b></p>
+      {/* not printing variable name */}
+      <p>trying to print name variable<b>{name}</b> </p>
+      {/* it will print variable name */}
+
+    </>
   );
 }
 
