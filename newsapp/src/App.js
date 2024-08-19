26a737e2d6fc487b6baa1eddc38fd85938cf4e71
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-08-19 12:50:10 +0530

    added infinity scroll

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -36,14 +36,14 @@ export default class App extends Component {
         <div>
           <Navbar mode={this.mode} theme={this.state.theme} />
           <Routes>
-            <Route path='/' element={<News key='general' pageSize={20} country="in" category="general" theme={this.state.theme} />} />
-            <Route path='/sports' element={<News key='sports' pageSize={20} country="in" category="sports" theme={this.state.theme} />} />
-            <Route path='/business' element={<News key='business' pageSize={20} country="in" category="business" theme={this.state.theme} />} />
-            <Route path='/general' element={<News key='general' pageSize={20} country="in" category="general" theme={this.state.theme} />} />
-            <Route path='/health' element={<News key='health' pageSize={20} country="in" category="health" theme={this.state.theme} />} />
-            <Route path='/entertainment' element={<News key='entertainment' pageSize={20} country="in" category="entertainment" theme={this.state.theme} />} />
-            <Route path='/science' element={<News key='science' pageSize={20} country="in" category="science" theme={this.state.theme} />} />
-            <Route path='/technology' element={<News key='technology' pageSize={20} country="in" category="technology" theme={this.state.theme} />} />
+            <Route path='/' element={<News key='general' pageSize={5} country="in" category="general" theme={this.state.theme} />} />
+            <Route path='/sports' element={<News key='sports' pageSize={5} country="in" category="sports" theme={this.state.theme} />} />
+            <Route path='/business' element={<News key='business' pageSize={5} country="in" category="business" theme={this.state.theme} />} />
+            <Route path='/general' element={<News key='general' pageSize={5} country="in" category="general" theme={this.state.theme} />} />
+            <Route path='/health' element={<News key='health' pageSize={5} country="in" category="health" theme={this.state.theme} />} />
+            <Route path='/entertainment' element={<News key='entertainment' pageSize={5} country="in" category="entertainment" theme={this.state.theme} />} />
+            <Route path='/science' element={<News key='science' pageSize={5} country="in" category="science" theme={this.state.theme} />} />
+            <Route path='/technology' element={<News key='technology' pageSize={5} country="in" category="technology" theme={this.state.theme} />} />
           </Routes>
         </div>
       </Router>
--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Loader from './Loader';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+
 export default class News extends Component {
     static defaultProps = {
         country: 'in',
@@ -19,17 +22,18 @@ export default class News extends Component {
         this.state = {
             articles: [],
             page: 1,
-            isLoading: false,
+            isLoading: true,
             totalResult: 1,
         }
         // if we dont bind this error is came with state
-        this.handlePrev = this.handlePrev.bind(this);
-        this.handleNext = this.handleNext.bind(this);
+        // this.handlePrev = this.handlePrev.bind(this);
+        // this.handleNext = this.handleNext.bind(this);
+        this.fetchMoreData = this.fetchMoreData.bind(this);
     }
 
     //creating all in fun
     async allInOneNewsFun() {
-        this.setState({ isLoading: true });
+        // this.setState({ isLoading: true });
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1feeb3ba971c425182492eef91ff97ce&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let rawData = await data.json();
@@ -39,27 +43,47 @@ export default class News extends Component {
             totalResult: rawData.totalResults || 0,
             isLoading: false
         });
+        document.title=`NewsMonkey- ${this.props.category}`;
     }
+
     //Called immediately after a component is mounted. Setting state here will trigger re-rendering.
     async componentDidMount() {
         this.allInOneNewsFun();
     }
-    //previous
-    async handlePrev() {
-        this.allInOneNewsFun();
-        this.setState({ page: this.state.page - 1 });
-    }
-    //next
-    async handleNext() {
-        this.allInOneNewsFun();
+
+    //fetch more data
+    fetchMoreData = async () => {
         this.setState({ page: this.state.page + 1 });
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1feeb3ba971c425182492eef91ff97ce&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let rawData = await data.json();
+        //added total results and articles
+        this.setState({
+            // changing aricles
+            articles: this.state.articles.concat(rawData.articles),
+            totalResult: rawData.totalResults || 0,
+            isLoading: false
+        });
     }
+
+    //?previous
+    // async handlePrev() {
+    //     this.allInOneNewsFun();
+    //     this.setState({ page: this.state.page - 1 });
+    // }
+
+    //?next
+    // async handleNext() {
+    //     this.allInOneNewsFun();
+    //     this.setState({ page: this.state.page + 1 });
+    // }
+
     render() {
         return (
             <>
                 <div className="container my-3">
                     <h1 className='text-center' style={{ margin: '30px 0px' }}>
-                        MonkeyNews - Top headlines
+                        MonkeyNews - Top {this.props.category} headlines
                     </h1>
                     <div className="container text-center">
                         {/* if loading then only show */}
@@ -68,20 +92,40 @@ export default class News extends Component {
 
                     {/* if loading is not true then only show */}
                     {
-                        !this.state.isLoading && <div className="row my-3">
-                            {this.state.articles.map((e) =>
-                                //Warning: Each child in a list should have a unique "key" prop.
-                                <div className="col-md-4" key={e.url}>
-                                    {/* if any null then show empty string */}
-                                    <NewsItem title={e.title ? e.title : ""} discription={e.description ? e.description : ""} imageUrl={e.urlToImage} url={e.url} author={e.author} time={e.publishedAt} source={e.source.name} theme={this.props.theme}/>
+                        // !this.state.isLoading && <div className="row my-3">
+                        <InfiniteScroll
+                            dataLength={this.state.articles.length}
+                            next={this.fetchMoreData}
+                            hasMore={this.state.articles.length <= this.state.totalResult}
+                            loader={<Loader />}
+                            endMessage={
+                                <p style={{ textAlign: "center" }}>
+                                    <b>Yay! You have seen it all</b>
+                                </p>
+                            }
+                        >
+                            <div className="container">
+                                <div className="row my-3">
+                                    {this.state.articles.map((e, index) =>
+                                        //Warning: Each child in a list should have a unique "key" prop.
+                                        <div className="col-md-4" key={`${e.url} - ${e.publishedAt}-${index}`}>
+                                            {/* if any null then show empty string */}
+                                            <NewsItem title={e.title ? e.title : ""} discription={e.description ? e.description : ""} imageUrl={e.urlToImage} url={e.url} author={e.author} time={e.publishedAt} source={e.source.name} theme={this.props.theme} />
+                                        </div>
+                                    )}
                                 </div>
-                            )}
-                        </div>
+                            </div>
+                        </InfiniteScroll>
                     }
-                    <div className="container d-flex justify-content-between">
-                        <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrev}>&larr; Previous</button>
-                        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResult / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNext}>Next &rarr;</button>
-                    </div>
+
+                    {
+                        /*
+                            <div className="container d-flex justify-content-between">
+                                <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrev}>&larr; Previous</button>
+                                <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResult / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNext}>Next &rarr;</button>
+                            </div>
+                         */
+                    }
                 </div>
             </>
         )
