18fb3ecbd417a585bb83065701d70348b9f7a983
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-09-04 19:47:09 +0530

    create note and fetch note using users unique id

--- inotebook/Backend/models/Notes.js
+++ inotebook/Backend/models/Notes.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
-const Schema  = mongoose.Schema;
+const Schema = mongoose.Schema;
+const User = require('./User');
 const notesSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,//linking user model using foregn key
+        ref: 'user',
+    },
     title: {
         type: String,
         required: true,
--- inotebook/Backend/routers/notes.js
+++ inotebook/Backend/routers/notes.js
@@ -1,8 +1,52 @@
 const express = require('express');
+const getUserData = require('../middleware/getuser');
+const Notes = require('../models/Notes');
 const router = express.Router();
+const { validationResult, body } = require('express-validator');
 
-router.get('/', (req, res) => {
-    res.send([]);
+//! getting note
+router.get('/getnote', getUserData, async (req, res) => {
+    try {
+        // get user id from middl. request
+        const uid = req.userId;
+        let note = await Notes.find({ user: uid });
+        res.send(note);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
 });
+
+//! create note
+router.post('/createnote', getUserData,
+    // validating body's parameters
+    [
+        body('title', 'title at least 2 char').isLength({ min: 2 }),
+        body('desciption', 'description at least 5 char').isLength({ min: 5 }),
+    ],
+    async (req, res) => {
+        // Extracts the validation errors of an express request
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        // if condition matches
+        try {
+            // get user id from middl. request
+            const uid = req.userId;
+            // destructuring req body
+            const { title, desciption, tag } = req.body;
+            // create
+            let note = new Notes({
+                title,
+                desciption,
+                tag,
+                user: uid,
+            });
+            const saved = await note.save();
+            res.send({ saved })
+        } catch (error) {
+            return res.status(500).send({ error: error.message });
+        }
+    });
 
 module.exports = router;
